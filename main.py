74bb855e9864307fe33cae877026e8089b3d4332
VitorMorgante <20233035895@estudantes.ifpr.edu.br>
2026-03-19 19:54:56 -0300

    refactor: finaliza a integraçao com Neon

--- main.py
+++ main.py
@@ -2,22 +2,20 @@ from database import iniciar_banco, salvar_registro
 
 def processar_dados(peso, area, comprimento):
     try:
-        iniciar_banco()
         salvar_registro(peso, area, comprimento)
-        
         return True
-    except Exception as e: 
+    except Exception as e:
         print(f"Erro no Armazenamento: {e}")
         return False
-        
-        
+
 if __name__ == "__main__":
-    #Dados passados na task
+    iniciar_banco()
+
     peso_teste = 420.0
     area_teste = 0.95
     comp_teste = 1.75
-    
-    print("Iniciando Armazenamento")
-    
+
+    print("Iniciando Armazenamento no Neon...")
+
     if processar_dados(peso_teste, area_teste, comp_teste):
-       print(f"Dados salvos {peso_teste}kg, {area_teste}m², {comp_teste}m")
+        print(f"Dados salvos {peso_teste}kg, {area_teste}m², {comp_teste}m")
