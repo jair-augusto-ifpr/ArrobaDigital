73400bf1ab1af555de70a2b7df21a8f99f5c2dac
VitorMorgante <20233035895@estudantes.ifpr.edu.br>
2026-03-19 19:47:42 -0300

    feat: migra a persistencia de dados para Neon PostgreSQL

--- database.py
+++ database.py
@@ -1,27 +1,38 @@
-import sqlite3
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+def conectar():
+    return psycopg2.connect(DATABASE_URL)
 
 def iniciar_banco():
-    conn = sqlite3.connect("cattle.db")
+    conn = conectar()
     cursor = conn.cursor()
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS measurements (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        weight REAL,
-        area REAL,
-        length REAL,
-        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-    )
+        CREATE TABLE IF NOT EXISTS measurements (
+            id SERIAL PRIMARY KEY,
+            weight REAL,
+            area REAL,
+            length REAL,
+            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
     """)
     conn.commit()
     conn.close()
-    
+
 def salvar_registro(peso, area, comprimento):
-    conn = sqlite3.connect("cattle.db")
-    cursor = conn.cursor()
-    cursor.execute("""
-        INSERT INTO measurements (weight, area, length)
-        VALUES (?, ?, ?)
-    """, (peso, area, comprimento))
-    conn.commit()
-    conn.close()
-    print(f"Dados salvos {peso}kg, {area}m², {comprimento}m")
+    try:
+        conn = conectar()
+        cursor = conn.cursor()
+        cursor.execute("""
+            INSERT INTO measurements (weight, area, length)
+            VALUES (%s, %s, %s)
+        """, (peso, area, comprimento))
+        conn.commit()
+        conn.close()
+        print(f"Dados salvos com sucesso no Neon: {peso}kg, {area}m², {comprimento}m")
+    except Exception as e:
+        print(f"Erro ao salvar no banco em nuvem: {e}")
